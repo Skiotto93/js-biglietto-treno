51027fc862d153d3cb0cc62997bec183ce81cea4
skiotto93 <fabio.forenza@hotmail.com>
2022-10-20 15:47:45 +0200

    Aggiunta 3a condizione

--- JS/main.js
+++ JS/main.js
@@ -2,13 +2,13 @@
     //prezzo del biglietto al km
 const priceAtKm = 0.21;
     //età passeggero     
-const age = 16;
+const age = 45;
     //distanza percorsa
 const distanceTraveled = 650;
 const grossPrice = (priceAtKm * distanceTraveled);
 console.log ("Prezzo lordo" + grossPrice + "€");
 // 2. va applicato uno sconto del 20% per i minorenni, va applicato uno sconto del 40% per gli over 65 
-if (age <18) {
+if (age < 18) {
     discount = 0.2;
     netDiscount = (grossPrice * discount);
     netPrice = (grossPrice - netDiscount);
@@ -16,11 +16,11 @@ if (age <18) {
     discount = 0.4;
     netDiscount = (grossPrice * discount);
     netPrice = (grossPrice - netDiscount);
-}    
-console.log ("Sconto" + netDiscount + "€");
-    else (age > 18) && (age < 65)  
+}   else if ( 18 < age && age > 65) {
     discount = 0 ;
     netPrice = (grossPrice);
+    } 
 
+console.log ("Sconto" + netDiscount + "€");
 console.log ("Prezzo netto" + netPrice + "€") 
 // Prezzo finale (con al massimo 2 numeri decimali)
